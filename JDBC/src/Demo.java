b97758b6dd9fc35fcd79f15bbd903d784d018928
Nirmalmahesh S <nirmalamhesh54@gmail.com>
2019-03-26 23:11:56 +0530

    Commit With executeupdate

--- JDBC/src/Demo.java
+++ JDBC/src/Demo.java
@@ -5,25 +5,21 @@ public class Demo {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		
-		String url = "jdbc:mysql://localhost:3306/db_example";
+		String url = "jdbc:sqlserver://localhost:1433; databaseName=Test";
 		String username = "sa";
 		String password = "joker";
      	//Load and Register the Driver	
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		//Establish the Connection
 		Connection con = DriverManager.getConnection(url, username, password);
 		//Create the Statment
 		Statement st = con.createStatement();
 		//Execute the query
-		ResultSet rs = st.executeQuery("select * from users");
+		//Execute Query Only returns the resultset oject but while insert data we have to know how many roes affected so we need to use executeUpdate Methord
+		int count = st.executeUpdate("insert into demo values('2','Ramesh','MBA')");
 		
-		//Process ResultSet
-	
-		while(rs.next())
-		{
-			System.out.println("Id:"+rs.getInt(1)+" Name:"+rs.getString(3));
-		}
-		
+		System.out.println(count+" Rows Affected");
+			
 		//Close Connection
 		st.close();
 		con.close();
